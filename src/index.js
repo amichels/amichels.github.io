8d9faf113ff0eabf2363b700ad36f640eb129a3f
Andy Michels <amichels@whitinghouse.com>
2018-02-04 22:42:43 -0600

    added back bquettebox.js settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import baguetteBox from 'baguettebox.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+baguetteBox.run('.js-portfolio');
